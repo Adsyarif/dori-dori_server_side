722061fc08e0eca9ce257baab71e68cfe88ada89
adrisa <adsyarif28@gmail.com>
2025-01-03 15:46:18 +0700

    feat: add login services

--- services/user-services/src/routes/userRoutes.ts
+++ services/user-services/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/userController";
+import { registerUser, loginUser } from "../controllers/userController";
 import asyncHandler from "../utils/asyncHandler";
 
 const userRouter = express.Router();
 
 userRouter.post("/uc/v1/register", asyncHandler(registerUser));
+userRouter.post("/uc/v1/login", asyncHandler(loginUser));
 
 export default userRouter;
